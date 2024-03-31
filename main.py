d6dbedf4a80489957627e48fd9a2366a98c620af
BoscoZZZ <94723520+BoscoZZZ@users.noreply.github.com>
2024-03-30 21:25:08 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-
+"""
+w
+"""
+import baseClass
 # Note: You may add helper functions, classes, etc. here as needed
 
 # Note: You may modify the code below as needed; the following starter template are just suggestions
 if __name__ == "__main__":
+    v = baseClass.load_graph(..., ...)
     print("Welcome to the Course Recommendation Service(UofT version)")
     name = input("Please enter your name to continue: ")
     print("hi, " + name + ". You can now enter your courses completed this year, ")
@@ -20,6 +24,23 @@ if __name__ == "__main__":
     print("Now, please enter your programme? ")
     print("if you are a year one, enter one programme you are interested in")
     choice = input("\nEnter programme: ").lower()
+    v = baseClass.Graph()
+    v.recommend_courses()
     programme_choice = choice
-
     print(choice)
+
+    recommendation = v.recommend_courses(lst, lst)
+
+    print("The recommendation course we would like to provides to you is follows")
+    print("We have recommend three courses for the each course you entered")
+    print(recommendation)
+
+    print("For further information on pre-requisite courses and co-requisite course,")
+    print("please view the following websites we provided")
+
+    website = ...
+
+    print(website)
+
+    print("Thank you for using Course Recommendation Service(UofT version)")
+    print("We sincerely wish you a best next school year")
